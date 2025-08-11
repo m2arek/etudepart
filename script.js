fd8defebe98d5ce4166974f3322956ac43ab232c
m2arek <p.maarek@gmail.com>
2025-08-11 09:19:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.getElementById("calculateIrrButton").addEventListener("click", async ()
   document.getElementById("facturean20").textContent = fr(facturean20);
 
   const angle = 35;
-  const url = `https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?outputformat=basic&lat=${gps[0]}&lon=${gps[1]}&raddatabase=PVGIS-SARAH2&peakpower=1&loss=14&pvtechchoice=crystSi&angle=${angle}&aspect=${orientation}&usehorizon=1`;
+  const url = `https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?outputformat=basic&lat=${gps[0]}&lon=${gps[1]}&raddatabase=PVGIS-SARAH2&peakpower=1&loss=10&pvtechchoice=crystSi&angle=${angle}&aspect=${orientation}&usehorizon=1`;
   const proxyUrl = `https://corsproxy.io/?url=${encodeURIComponent(url)}`;
 
   try {
